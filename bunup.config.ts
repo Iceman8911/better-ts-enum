3abdb28f46147f1a729b5bf125b7833577d79f12
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-15 23:48:41 +0100

    build(library, config): add minified exports

--- bunup.config.ts
+++ bunup.config.ts
@@ -8,9 +8,23 @@ export default defineConfig([
 		format: ["esm", "cjs"],
 	},
 	{
+		entry: "src/basic-enum.ts",
+		outDir: "dist/min/basic-enum",
+		name: "min/basic-enum",
+		format: ["esm", "cjs"],
+		minify: true,
+	},
+	{
 		entry: "src/arithmetic.ts",
 		name: "arithmetic",
 		format: ["esm", "cjs"],
 		outDir: "dist/arithmetic",
+	},
+	{
+		entry: "src/arithmetic.ts",
+		name: "min/arithmetic",
+		format: ["esm", "cjs"],
+		outDir: "dist/min/arithmetic",
+		minify: true,
 	},
 ]);
