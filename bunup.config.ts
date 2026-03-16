24773cb1e7fb3afb0b5b39f838e6383c54f94b48
Iceman8911 <wuchiJSS3@gmail.com>
2026-03-16 17:32:54 +0100

    build(library, config): add browser iife builds

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from "bunup";
 
-export default defineConfig({
-	entry: "src/*.ts",
-	format: ["esm", "cjs"],
-	exports: true,
-	packages: "external",
-});
+export default defineConfig([
+	{
+		name: "bundler",
+		entry: "src/*.ts",
+		format: ["esm", "cjs"],
+		exports: true,
+		packages: "external",
+		report: { brotli: true, gzip: true },
+	},
+	{
+		name: "browser",
+		entry: "src/*.ts",
+		format: "iife",
+		packages: "bundle",
+		sourcemap: "linked",
+		target: "browser",
+		minify: true,
+		clean: false,
+		report: { brotli: true, gzip: true },
+	},
+]);
